f7e9c4e8b02bc447cfd42bb2ef429a65981beb07
Martin Laurence Coloma <cmartinlaurence@gmail.com>
2025-10-28 19:39:41 +0800

    render | routing 2

--- api/index.js
+++ api/index.js
@@ -5,7 +5,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import attendanceRoutes from "./routes/attendance.js";
-import usersRoutes from "./routes/users.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,7 +14,6 @@ app.use(express.json());
 
 // Routes
 app.use("/attendance", attendanceRoutes);
-app.use("/users", usersRoutes);
 
 // Root endpoint
 app.get("/", (req, res) => {
